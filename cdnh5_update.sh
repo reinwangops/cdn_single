dfe471e1235418eeb52a7bdad0335c02cf5bd4e7
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-28 12:56:37 +0800

    Create cdnh5_update.sh

---
+++ cdnh5_update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SRC_PATH=/home/development/front
+SRC_PATH=/home/development/new_h5/replace_prod
+PRD_PATH=/home/nginx/web/replace
+PKG_NAME=distcdn.zip
+SRC_NAME=distcdn
+tartime=`date "+%Y%m%d_%H%M%S"`
+
+#check backup dir
+if [ ! -d "$SRC_PATH/backup" ]; then
+  mkdir $SRC_PATH/backup
+fi
+
+#check nginx dir
+if [ ! -d "$PRD_PATH" ]; then
+  mkdir $PRD_PATH
+fi
+
+#copy && backup
+\cp -rf $SRC_PATH/$PKG_NAME $PRD_PATH
+\cp -rf $SRC_PATH/$PKG_NAME $SRC_PATH/backup/$PKG_NAME\_$tartime
+
+#rollbackneed && update
+cd $PRD_PATH
+
+rm -rf bk-front
+\mv -f front bk-front
+unzip $PKG_NAME
+\mv -f $SRC_NAME front
+rm -rf  front/index.html
